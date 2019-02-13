dfcf60eb7c597ea52315bcaaf15f9e828c939a05
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2019-02-13 01:45:18 +0000

    obtainng max canvas height and wdth using window.innerHeight and window.innerWidth

--- uploadFile.js
+++ uploadFile.js
@@ -3,8 +3,8 @@ imageUploaded = false;
 function readURL(input) {
        removeBndBxIfLabelNamePending();
        if (input.files && input.files[0]) {
-           let maximumCanvasHeight = window.screen.height - 160;
-           let maximumCanvasWidth = window.screen.width - 160;
+           let maximumCanvasHeight = window.innerHeight - 54;
+           let maximumCanvasWidth = window.innerWidth - 110;
            var reader = new FileReader();
            imageName = input.files[0].name;
            reader.onload = function (e) {
@@ -34,11 +34,6 @@ function readURL(input) {
                  let widthRatio = maximumCanvasWidth / this.width;
                  canvas2.width = maximumCanvasWidth;
                  canvas2.height = canvas2.height * widthRatio;
-                 if(maximumCanvasHeight < canvas2.height){
-                   let heightRatio = maximumCanvasHeight / this.height;
-                   canvas2.height = maximumCanvasHeight;
-                   canvas2.width = this.width * heightRatio;
-                 }
                  canvas2.getContext("2d").drawImage(this, 0, 0, canvas2.width, canvas2.height);
                  canvas.setWidth(canvas2.width);
                  canvas.setHeight(canvas2.height);
