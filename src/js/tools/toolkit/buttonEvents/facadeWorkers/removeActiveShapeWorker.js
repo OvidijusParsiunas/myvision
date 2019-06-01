f7967577011be5a0f186b3ccff6f9a9efed60c27
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2019-06-01 01:57:40 +0100

    when adding new points to polygon and deleting shape, set back to the selection of polygon to add state

--- src/js/tools/toolkit/buttonEvents/facadeWorkers/removeActiveShapeWorker.js
+++ src/js/tools/toolkit/buttonEvents/facadeWorkers/removeActiveShapeWorker.js
@@ -3,9 +3,18 @@ import {
 } from '../../../../canvas/objects/polygon/alterPolygon/alterPolygon';
 import { clearPolygonData } from '../../../../canvas/objects/polygon/polygon';
 import { removeEditedPolygonId } from '../../../../canvas/mouseInteractions/mouseEvents/eventWorkers/editPolygonEventsWorker';
+import purgeCanvasMouseEvents from '../../../../canvas/mouseInteractions/mouseEvents/resetCanvasUtils/purgeAllMouseHandlers';
+import assignAddPointsOnExistingPolygonEvents from '../../../../canvas/mouseInteractions/mouseEvents/eventHandlers/addPointsEventHandlers';
+import setInitialStageOfAddPointsOnExistingPolygonMode from '../../../../canvas/mouseInteractions/cursorModes/initialiseAddPointsOnExistingPolygonMode';
+import { getAddingPolygonPointsState } from '../facadeWorkersUtils/stateManager';
 
 function initiateActiveShapeEvent(canvas) {
   if (isAddingPointsToPolygon()) {
+    purgeCanvasMouseEvents(canvas);
+    assignAddPointsOnExistingPolygonEvents(canvas);
+    clearAllAddPointsData();
+    setInitialStageOfAddPointsOnExistingPolygonMode(canvas);
+  } else if (getAddingPolygonPointsState()) {
     clearAllAddPointsData();
   }
   if (canvas.getActiveObject()) {
