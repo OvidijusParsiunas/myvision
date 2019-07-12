b19cec2d42c487aeca05cec088ee0fdee710bd19
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2019-07-12 23:10:50 +0100

    added functionality to attach a label to the shape

--- src/js/tools/labellerPopUp/labellingProcess.js
+++ src/js/tools/labellerPopUp/labellingProcess.js
@@ -1,3 +1,4 @@
+import fabric from 'fabric';
 import { resetObjectCursors, waitingForLabelCursorMode } from '../../canvas/mouseInteractions/cursorModes/drawMode';
 import { getLabelPopUpText, hideLabelPopUp } from './style';
 
@@ -20,9 +21,54 @@ function removeTargetShape() {
   labellingState = false;
 }
 
+function getTextProperties(shape) {
+  return {
+    fontSize: 10,
+    fill: 'yellow',
+    left: shape.left,
+    top: shape.top,
+    width: shape.width,
+    height: shape.height,
+  };
+}
+
+
+function generateText(text) {
+  // generate text shape
+  const textShape = new fabric.Text(text, getTextProperties(targetShape));
+  // setting bndbox group
+  if (targetShape.shapeName === 'bndBox') {
+    const group = new fabric.Group([targetShape, textShape]);
+    canvas.add(group);
+    // setting polygin group
+  } else if (targetShape.shapeName === 'polygon') {
+    // const group = new fabric.Group([targetShape, textShape], polygonProperties.newPolygon);
+    // canvas.add(group);
+  }
+  canvas.remove(targetShape);
+}
+
+// function generateText(text) {
+//   // generate text shape
+//   const textShape = new fabric.Text(text, getTextProperties(targetShape));
+//
+//   // setting bndbox group
+//   if (targetShape.shapeName === 'bndBoxTemp') {
+//     const group = new fabric.Group([textShape]);
+//     group.addWithUpdate(targetShape);
+//     canvas.add(group);
+//     // setting polygin group
+//   } else if (targetShape.shapeName === 'polygon') {
+//     // const group = new fabric.Group([targetShape, textShape], polygonProperties.newPolygon);
+//     // canvas.add(group);
+//   }
+//   // canvas.remove(targetShape);
+// }
+
 function createLabelShape() {
   const text = getLabelPopUpText();
   hideLabelPopUp();
+  generateText(text);
   resetObjectCursors(canvas);
   targetShape.set('id', currentId);
   currentId += 1;
