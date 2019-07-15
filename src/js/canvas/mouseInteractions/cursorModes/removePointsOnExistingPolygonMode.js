340df834769448ce0935a96b2bfa27f1c762e1d0
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2019-07-15 22:08:12 +0100

    when in any state, and clicked to remove points, all objects hover cursor is set to default immediately

--- src/js/canvas/mouseInteractions/cursorModes/removePointsOnExistingPolygonMode.js
+++ src/js/canvas/mouseInteractions/cursorModes/removePointsOnExistingPolygonMode.js
@@ -1,12 +1,9 @@
 import { setObjectsHoverCursorToDefault } from '../../objects/objectsProperties/changeProperties';
-import { getContinuousDrawingState } from '../../../tools/toolkit/buttonEvents/facadeWorkersUtils/stateManager';
 
 function setRemovePointsOnExistingPolygonMode(canvas) {
   canvas.defaultCursor = 'default';
   canvas.hoverCursor = 'default';
-  if (getContinuousDrawingState()) {
-    setObjectsHoverCursorToDefault(canvas);
-  }
+  setObjectsHoverCursorToDefault(canvas);
 }
 
 export { setRemovePointsOnExistingPolygonMode as default };
