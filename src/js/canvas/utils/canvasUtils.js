944e5f7125e62a2199cfd85dbc14db916d380886
ovi <oparsiunas@googlemail.com>
2019-10-16 08:24:38 -0400

    set a timeout of canvaswrapperinnerelement changes for firefox to prevent flashing

--- src/js/canvas/utils/canvasUtils.js
+++ src/js/canvas/utils/canvasUtils.js
@@ -3,12 +3,25 @@ let oldCanvas = null;
 let canvas = null;
 let canvasElement = null;
 let canvasElement2 = null;
+let timeoutMilliseconds = 0;
+
+function isFirefox() {
+  if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+    return true;
+  }
+  return false;
+}
+
+function assignTimeoutMillisecondsDependingOnBrowser() {
+  timeoutMilliseconds = isFirefox() ? 12 : 0;
+}
 
 function assignCanvasForUtils(canvasObj) {
   canvas = canvasObj;
   canvas.randomProperty = 'test';
   canvasElement = document.getElementById('canvas-wrapper-inner');
   canvasElement2 = document.getElementById('canvas-wrapper-inner2');
+  assignTimeoutMillisecondsDependingOnBrowser();
 }
 
 function enableActiveObjectsAppearInFront(canvas) {
@@ -29,10 +42,10 @@ function preventActiveObjectsAppearInFront(canvas) {
 function switchCanvasWrapperInnerElementsDisplay() {
   setTimeout(() => {
     if (displaying) {
-      canvasElement.style.left = '50%';
-      canvasElement.style.top = '50%';
       canvasElement2.style.display = '';
       canvasElement.style.display = 'none';
+      canvasElement.style.left = '50%';
+      canvasElement.style.top = '50%';
       displaying = false;
     } else {
       canvasElement.style.display = '';
@@ -43,7 +56,7 @@ function switchCanvasWrapperInnerElementsDisplay() {
     }
     oldCanvas.setViewportTransform([1, 0, 0, 1, 0, 0]);
     oldCanvas.clear();
-  }, 0);
+  }, timeoutMilliseconds);
 }
 
 export {
