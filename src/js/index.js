015bd92a02bfc54f16b2d750d824ccbb901eed55
ovi <oparsiunas@googlemail.com>
2019-10-23 10:57:28 -0400

    the sidebar properties are set up using css

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
-import setupWindowLayout from './tools/styling/setup';
+import initialiseWindowLayoutSetup from './tools/styling/setup';
 import { constructCanvas } from './canvas/canvas';
 import initialiseLabellerPopUp from './tools/labellerPopUp/buttons';
 import assignToolkitButtonEvents from './tools/toolkit/buttons';
 import { initialiseLabelListFunctionality } from './tools/labelList/labelList';
 import { initialiseImageListFunctionality } from './tools/imageList/imageList';
 
-setupWindowLayout();
 constructCanvas();
 initialiseLabellerPopUp();
 assignToolkitButtonEvents();
 initialiseLabelListFunctionality();
 initialiseImageListFunctionality();
+initialiseWindowLayoutSetup();
--- src/js/tools/styling/setup.js
+++ src/js/tools/styling/setup.js
@@ -8,17 +8,19 @@ function findElements() {
   zoomOverflowWrapperParentElement = document.getElementById('zoom-overflow-wrapper-parent');
 }
 
-function addPropertiesToElements() {
-  canvasWrapperParentElement.style.width = `calc(100% - ${rightSideBar.width + leftSideBar.width}px)`;
-  // could be the reason for uneven results
-  zoomOverflowWrapperParentElement.style.width = `calc(100% - ${rightSideBar.width + leftSideBar.width + 1}px)`;
-  canvasWrapperParentElement.style.left = `${leftSideBar.width}px`;
-  zoomOverflowWrapperParentElement.style.left = `${leftSideBar.width}px`;
-}
+// this will be used to resize the side-bars later on
+// function addPropertiesToElements() {
+//   // remove this if the initial flashing will be too much to take in
+//   canvasWrapperParentElement.style.width = `calc(100% - ${rightSideBar.width + leftSideBar.width}px)`;
+//   // could be the reason for uneven results
+//   zoomOverflowWrapperParentElement.style.width = `calc(100% - ${rightSideBar.width + leftSideBar.width + 1}px)`;
+//   canvasWrapperParentElement.style.left = `${leftSideBar.width}px`;
+//   zoomOverflowWrapperParentElement.style.left = `${leftSideBar.width}px`;
+// }
 
-function setupWindowLayout() {
+function initialiseWindowLayoutSetup() {
   findElements();
-  addPropertiesToElements();
+  // addPropertiesToElements();
 }
 
-export { setupWindowLayout as default };
+export { initialiseWindowLayoutSetup as default };
