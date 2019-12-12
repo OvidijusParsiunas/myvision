72b450d11e5a324d18dfbd0f812c4e7ddbb58a60
ovi <oparsiunas@googlemail.com>
2019-12-11 23:51:34 -0500

    created the popup for exporting labels

--- src/js/tools/toolkit/buttonClickEvents/facadeWorkers/downloadAnnotationsFileWorker.js
+++ src/js/tools/toolkit/buttonClickEvents/facadeWorkers/downloadAnnotationsFileWorker.js
@@ -1,6 +1,13 @@
 // import { downloadXML } from '../facadeWorkersUtils/downloadFile/fileTypes/XML';
 import downloadYOLOTXT from '../facadeWorkersUtils/downloadFile/fileTypes/YOLOTXT';
 
+// function showLabelPopUp() {
+// dimWindow();
+// popupLabelParentElement = document.getElementById('popup-label-parent');
+// popupLabelParentElement.style.display = 'block';
+// validateFullPopUpVisibile();
+// }
+
 function downloadTrainingDataEvent(canvas) {
   canvas.discardActiveObject();
   downloadYOLOTXT();
