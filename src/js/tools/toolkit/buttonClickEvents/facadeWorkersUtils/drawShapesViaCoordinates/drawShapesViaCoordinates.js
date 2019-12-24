6c590e8224f1b007f747f425f3749f6c2b5cc117
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2019-12-24 12:03:46 +0000

    refactored the bounding box generation via ML code

--- src/js/tools/toolkit/buttonClickEvents/facadeWorkersUtils/drawShapesViaCoordinates/drawShapesViaCoordinates.js
+++ src/js/tools/toolkit/buttonClickEvents/facadeWorkersUtils/drawShapesViaCoordinates/drawShapesViaCoordinates.js
@@ -10,30 +10,16 @@ import { getCurrentImageId } from '../stateManager';
 
 let canvas = null;
 
-function captureCurrentImageData(allImageData, currentlySelectedImageId) {
-  const currentlySelectedImageProperties = getImageProperties();
-  const imageDimensions = {};
-  imageDimensions.scaleX = currentlySelectedImageProperties.scaleX;
-  imageDimensions.scaleY = currentlySelectedImageProperties.scaleY;
-  allImageData[currentlySelectedImageId].imageDimensions = imageDimensions;
-}
-
-function getImageDimensions(image) {
-  if (image && image.imageDimensions && Object.keys(image.imageDimensions).length > 0) {
-    return image.imageDimensions;
+function updateThumbnailStyle(isCurrentlySelectedImage, image) {
+  if (!isCurrentlySelectedImage) {
+    highlightImageThumbnailForML(image.thumbnailElementRef);
+  } else {
+    highlightCurrentImageThumbnailForML(image.thumbnailElementRef);
   }
-  return { scaleX: 1, scaleY: 1 };
 }
 
-function generateNewBoundingBox(shapeCoordinates, imageDimensions, image,
-  isCurrentlySelectedImage, isUsingMachineLearning) {
-  const boundingBoxShape = createNewBoundingBoxFromCoordinates(
-    shapeCoordinates.bbox[0],
-    shapeCoordinates.bbox[1],
-    shapeCoordinates.bbox[2],
-    shapeCoordinates.bbox[3],
-    imageDimensions,
-  );
+function newLabelShapeGroup(boundingBoxShape, shapeCoordinates,
+  isCurrentlySelectedImage, isUsingMachineLearning, image) {
   if (isCurrentlySelectedImage) {
     generateLabelShapeGroup(boundingBoxShape, shapeCoordinates.class,
       null, isUsingMachineLearning);
@@ -44,19 +30,33 @@ function generateNewBoundingBox(shapeCoordinates, imageDimensions, image,
   }
 }
 
-function generateNewShapes(image, imageId, currentlySelectedImageId,
-  predictedShapeCoordinates, imageDimensions, isUsingMachineLearning) {
-  const isCurrentlySelectedImage = currentlySelectedImageId === parseInt(imageId, 10);
+function generateNewBoundingBox(shapeCoordinates, imageDimensions) {
+  const boundingBoxShape = createNewBoundingBoxFromCoordinates(
+    shapeCoordinates.bbox[0],
+    shapeCoordinates.bbox[1],
+    shapeCoordinates.bbox[2],
+    shapeCoordinates.bbox[3],
+    imageDimensions,
+  );
+  return boundingBoxShape;
+}
+
+function generateNewShapes(image, isCurrentlySelectedImage, predictedShapeCoordinates,
+  imageDimensions, isUsingMachineLearning) {
   predictedShapeCoordinates.forEach((shapeCoordinates) => {
-    generateNewBoundingBox(shapeCoordinates, imageDimensions, image,
+    const boundingBox = generateNewBoundingBox(shapeCoordinates, imageDimensions, image,
       isCurrentlySelectedImage, isUsingMachineLearning);
+    newLabelShapeGroup(boundingBox, shapeCoordinates, isCurrentlySelectedImage,
+      isUsingMachineLearning, image);
   });
-  if (!isCurrentlySelectedImage) {
-    highlightImageThumbnailForML(image.thumbnailElementRef);
-  } else {
-    highlightCurrentImageThumbnailForML(image.thumbnailElementRef);
-  }
   image.numberOfMLGeneratedShapes = predictedShapeCoordinates.length;
+}
+
+function getImageDimensions(image) {
+  if (image && image.imageDimensions && Object.keys(image.imageDimensions).length > 0) {
+    return image.imageDimensions;
+  }
+  return { scaleX: 1, scaleY: 1 };
 }
 
 function generateNewShapesForImages(predictedShapeCoordinatesForImages, allImageData,
@@ -64,16 +64,26 @@ function generateNewShapesForImages(predictedShapeCoordinatesForImages, allImage
   Object.keys(predictedShapeCoordinatesForImages).forEach((key) => {
     const image = allImageData[key];
     const imageDimensions = getImageDimensions(image);
+    const isCurrentlySelectedImage = currentlySelectedImageId === parseInt(key, 10);
     const predictedShapeCoordinates = predictedShapeCoordinatesForImages[key];
     if (predictedShapeCoordinates.length > 0) {
-      generateNewShapes(image, key, currentlySelectedImageId,
-        predictedShapeCoordinates, imageDimensions, isUsingMachineLearning);
+      generateNewShapes(image, isCurrentlySelectedImage, predictedShapeCoordinates,
+        imageDimensions, isUsingMachineLearning);
+      updateThumbnailStyle(isCurrentlySelectedImage, image);
     }
   });
 }
 
+function captureCurrentImageData(allImageData, currentlySelectedImageId) {
+  const currentlySelectedImageProperties = getImageProperties();
+  const imageDimensions = {};
+  imageDimensions.scaleX = currentlySelectedImageProperties.scaleX;
+  imageDimensions.scaleY = currentlySelectedImageProperties.scaleY;
+  allImageData[currentlySelectedImageId].imageDimensions = imageDimensions;
+}
+
 function drawShapesViaCoordinates(predictedShapeCoordinatesForImages, isUsingMachineLearning) {
-  predictedShapeCoordinatesForImages = predictedShapeCoordinatesForImages = {"1":[{"bbox":[0.23196187615394592,1.3171005249023438,282.11527583003044,337.3044550418854],"class":"cat","score":0.8860134482383728}],"1":[{"bbox":[16.03703498840332,194.2115306854248,1113.8134002685547,482.022762298584],"class":"car","score":0.9936941266059875},{"bbox":[1233.7510585784912,1169.7566986083984,1080.3159713745117,385.3567123413086],"class":"car","score":0.9841077327728271},{"bbox":[96.5882420539856,1009.1146469116211,1040.1406645774841,506.1511993408203],"class":"truck","score":0.9241188764572144},{"bbox":[1270.0901985168457,110.06307601928711,1079.1927337646484,524.1976737976074],"class":"car","score":0.8551244735717773}]};
+  predictedShapeCoordinatesForImages = predictedShapeCoordinatesForImages = {"0":[{"bbox":[0.23196187615394592,1.3171005249023438,282.11527583003044,337.3044550418854],"class":"cat","score":0.8860134482383728}],"1":[{"bbox":[16.03703498840332,194.2115306854248,1113.8134002685547,482.022762298584],"class":"car","score":0.9936941266059875},{"bbox":[1233.7510585784912,1169.7566986083984,1080.3159713745117,385.3567123413086],"class":"car","score":0.9841077327728271},{"bbox":[96.5882420539856,1009.1146469116211,1040.1406645774841,506.1511993408203],"class":"truck","score":0.9241188764572144},{"bbox":[1270.0901985168457,110.06307601928711,1079.1927337646484,524.1976737976074],"class":"car","score":0.8551244735717773}]};
   isUsingMachineLearning = true;
   const currentlySelectedImageId = getCurrentImageId();
   const allImageData = getAllImageData();
