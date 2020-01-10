17de6a6364c712cd2e9ec5bca64f8581b31a36d0
ovi <oparsiunas@googlemail.com>
2020-01-09 20:27:11 -0500

    added event handler to the instantiate ml popup cancel button

--- src/js/tools/machineLearningPopUp/views/initiateMachineLearning/buttonEvents.js
+++ src/js/tools/machineLearningPopUp/views/initiateMachineLearning/buttonEvents.js
@@ -1,9 +1,11 @@
 import startMachineLearning from './machineLearning';
+import { closeMachineLearningPopUp } from './style';
 
 function registerButtonEventHandlers(nextViewCallback, setMachineLearningData) {
   window.startMachineLearning = startMachineLearning.bind(
     this, nextViewCallback, setMachineLearningData,
   );
+  window.closeMachineLearningPopUp = closeMachineLearningPopUp;
 }
 
 export { registerButtonEventHandlers as default };
