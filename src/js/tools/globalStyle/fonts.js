70703021adac2b5ac10e86c63f3036ffcba3c8c2
ovi <oparsiunas@googlemail.com>
2020-02-22 17:30:19 -0500

    fixed a bug where the elements size in firefox would not be re-rendered on screen size change

--- src/js/tools/globalStyle/fonts.js
+++ src/js/tools/globalStyle/fonts.js
@@ -22,16 +22,31 @@ function loadFailed() {
   console.log('Failed to load custom fonts');
 }
 
+// should be a global variable
+function isFirefox() {
+  return navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+}
+
+// fix for a bug where the loading of the script would stop all elements from being
+// dynamic and the browser would not render them when screen dimensions are changed
+function firefoxBugFix(document, url, relationship) {
+  const div = document.createElement('div');
+  div.innerHTML = `<link rel="${relationship}" href="${url}">`;
+  document.head.appendChild(div);
+}
+
 function downloadFonts() {
   // potential alternative
   // <link href="https://fonts.googleapis.com/css?family=Alef|Archivo|Average|Barlow+Semi+Condensed|Basic|Cantarell|Chivo|Hind+Madurai|IBM+Plex+Serif|K2D|M+PLUS+1p|Mada|Palanquin|Pavanam|Source+Sans+Pro|Yantramanav&display=swap" rel="stylesheet">
   const url = 'https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap';
+  const relationship = 'stylesheet';
   const link = document.createElement('link');
   link.onload = loadSuccess;
   link.onerror = loadFailed;
+  link.rel = relationship;
   link.href = url;
-  link.rel = 'stylesheet';
   document.head.appendChild(link);
+  if (isFirefox()) firefoxBugFix(document, url, relationship);
 }
 
 export { downloadFonts as default };
