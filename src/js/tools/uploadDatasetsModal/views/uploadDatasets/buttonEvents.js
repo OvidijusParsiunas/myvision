de0d778c146fcc8e1a18e5786a96b88ee8b40064
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2020-03-04 19:39:40 +0000

    change the upload dataset button trigger button naming convention@

--- src/js/tools/uploadDatasetsModal/views/uploadDatasets/buttonEvents.js
+++ src/js/tools/uploadDatasetsModal/views/uploadDatasets/buttonEvents.js
@@ -1,14 +1,19 @@
 import { hideUploadDatasetsViewAssets } from './style';
 
-function moveToNextView(nextViewCallback) {
-  hideUploadDatasetsViewAssets();
-  nextViewCallback();
+// function moveToNextView(nextViewCallback) {
+//   hideUploadDatasetsViewAssets();
+//   nextViewCallback();
+// }
+
+function triggerDatasetUpload() {
+  document.getElementById('upload-datasets-modal-upload-datasets-upload-trigger').click();
 }
 
 function registerButtonEventHandlers(nextViewCallback) {
   // window.startUploadDatasets = moveToNextView.bind(
   //   this, nextViewCallback,
   // );
+  window.triggerDatasetUpload = triggerDatasetUpload;
 }
 
 export { registerButtonEventHandlers as default };
