3008ea394330a55e923da3d38a384f3156ed5e67
ovi <oparsiunas@googlemail.com>
2020-03-10 23:42:24 +0000

    refactored categories array check and added annotations array check

--- src/js/tools/uploadDatasetsModal/views/uploadDatasets/formatValidators/COCOJSONValidator.js
+++ src/js/tools/uploadDatasetsModal/views/uploadDatasets/formatValidators/COCOJSONValidator.js
@@ -19,19 +19,47 @@ function checkImagesProperties() {
     
 }
 
-function checkAnnotationsProperties() {
+function checkProperties(requiredProperties, subjectObject) {
+  const nullProperties = requiredProperties.filter(
+    property => subjectObject[property] === undefined,
+  );
+  if (nullProperties.length > 0) {
+    return { error: true, message: `The following properties have not been found: ${nullProperties}` };
+  }
+  return { error: false, message: '' };
+}
 
+function checkAnnotationsProperties(parsedObj) {
+  const requiredProperties = ['id', 'image_id', 'category_id', 'segmentation', 'area', 'bbox'];
+  const { annotations } = parsedObj;
+  if (!Array.isArray(annotations)) {
+    return { error: true, message: 'annotations property is not an array' };
+  }
+  for (let i = 0; i < annotations.length; i += 1) {
+    const result = checkProperties(requiredProperties, annotations[i]);
+    if (result.error) {
+      result.message += ' -> in annotations';
+      return result;
+    }
+    if (!Array.isArray(annotations[i].bbox)
+      || annotations[i].bbox.filter(entry => typeof entry !== 'number').length > 0) {
+      return { error: true, message: 'bbox array is incorrect -> in annotations' };
+    }
+  }
+  return { error: false, message: '' };
 }
 
 function checkCategoriesProperties(parsedObj) {
   const requiredProperties = ['id', 'name'];
   const { categories } = parsedObj;
+  if (!Array.isArray(categories)) {
+    return { error: true, message: 'categories property is not an array' };
+  }
   for (let i = 0; i < categories.length; i += 1) {
-    const nullProperties = requiredProperties.filter(
-      property => categories[i][property] === undefined,
-    );
-    if (nullProperties.length > 0) {
-      return { error: true, message: '' };
+    const result = checkProperties(requiredProperties, categories[i]);
+    if (result.error) {
+      result.message += ' -> in categories';
+      return result;
     }
   }
   return { error: false, message: '' };
@@ -39,11 +67,7 @@ function checkCategoriesProperties(parsedObj) {
 
 function checkParentProperties(parsedObj) {
   const requiredProperties = ['images', 'annotations', 'categories'];
-  const nullProperties = requiredProperties.filter(property => parsedObj[property] === undefined);
-  if (nullProperties.length === 0) {
-    return { error: false, message: '' };
-  }
-  return { error: true, message: `The following properties have not been found: ${nullProperties}` };
+  return checkProperties(requiredProperties, parsedObj);
 }
 
 function checkJONObject(JSONObject, validators) {
@@ -59,7 +83,7 @@ function checkJONObject(JSONObject, validators) {
 function validateCOCOJSONFormat(parsedObj) {
   if (parsedObj.fileFormat === 'annotations') {
     console.log(parsedObj);
-    checkJONObject(parsedObj.body, [checkParentProperties, checkCategoriesProperties]);
+    checkJONObject(parsedObj.body, [checkParentProperties, checkCategoriesProperties, checkAnnotationsProperties]);
   }
 }
 
