2d3c614509f41bf778199e2c914b4da25dd61cc3
ovi <oparsiunas@googlemail.com>
2020-06-24 18:42:10 +0000

    added favicon

--- src/js/canvas/utils/watermark.js
+++ src/js/canvas/utils/watermark.js
@@ -4,7 +4,12 @@ function drawWatermarkOnCanvasAreaBackground() {
   if (watermarkDisplayed) return;
   const canvasWrapperParentElement = document.getElementById('canvas-wrapper-parent');
   canvasWrapperParentElement.style.backgroundImage = 'url(\'presenting 32.svg\')';
-  /*   background: url(presenting\ 9.svg); */
+  /*
+    background: url(presenting\ 9.svg);
+    background: url(presenting\ 30.svg);
+    background: url(presenting\ 31.svg);
+    background: url(presenting\ 32.svg);
+  */
   canvasWrapperParentElement.style.backgroundAttachment = 'fixed';
   canvasWrapperParentElement.style.backgroundPosition = 'center';
   canvasWrapperParentElement.style.backgroundPositionX = 'calc((100% - 210px - (59px / var(--screen-size-delta))) / 2 + 70px)';
