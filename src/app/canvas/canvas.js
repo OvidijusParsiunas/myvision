00cd726d70bff994058dd9bda7c77fe065dbb8ac
ovi <oparsiunas@googlemail.com>
2020-07-29 20:33:07 +0000

    retrigger deploy

--- src/app/canvas/canvas.js
+++ src/app/canvas/canvas.js
@@ -1,29 +1,9 @@
-import { assignCanvasForDrawingShapesViaCoordinates } from './utils/drawShapesViaCoordinates/drawShapesViaCoordinates';
-import { assignCanvasMouseEvents } from '../tools/toolkit/buttonClickEvents/facade';
-import assignResetCanvasEventsFuncToMouseEvents from '../tools/toolkit/buttonClickEvents/facadeWorkersUtils/resetCanvasEvents';
 import { setBoundingBoxEditToolsToBeTransparent, createNewCanvas, reasignCanvas } from './utils/fabricUtils';
-import { assignCanvasForLabelManipulation } from './objects/label/label';
-import { assignCanvasForShapeFillManipulation } from './objects/allShapes/allShapes';
-import { assignCanvasForResettingToDefaultAfterAddPoints } from './mouseInteractions/mouseEvents/resetCanvasUtils/resetCanvasAfterAddPoints';
-import assignCanvasForResizeWhenWindowResize from '../tools/globalStyling/windowResize';
-import { assignCanvasForLabelAndShapeBuilder } from './objects/allShapes/labelAndShapeBuilder';
-import { assignCanvasToDimWindowService } from '../tools/dimWindow/dimWindowService';
 import { assignCanvasForUtils, assignNewCanvasForUtils } from './utils/canvasUtils';
-import { assignCanvasForHotKeys } from '../keyEvents/keyboard/hotKeys';
 
 let currentCanvasInstance = null;
 
-function repopulateCanvasReference(canvas) {
-  assignCanvasMouseEvents(canvas);
-  assignCanvasForHotKeys(canvas);
-  assignCanvasToDimWindowService(canvas);
-  assignCanvasForLabelManipulation(canvas);
-  assignCanvasForShapeFillManipulation(canvas);
-  assignResetCanvasEventsFuncToMouseEvents(canvas);
-  assignCanvasForResettingToDefaultAfterAddPoints(canvas);
-  assignCanvasForDrawingShapesViaCoordinates(canvas);
-  assignCanvasForResizeWhenWindowResize(canvas);
-  assignCanvasForLabelAndShapeBuilder(canvas);
+function repopulateCanvasReference() {
 }
 
 function constructCanvas() {
