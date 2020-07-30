aabfc6eef15d6c820cdd04a54b7ed637a775663b
ovi <oparsiunas@googlemail.com>
2020-07-30 17:37:34 +0000

    undoing previous changes to deploy to the public folder without deleting the existing bundles

--- src/app/canvas/canvas.js
+++ src/app/canvas/canvas.js
@@ -1,9 +1,35 @@
+import { assignCanvasForDrawingShapesViaCoordinates } from './utils/drawShapesViaCoordinates/drawShapesViaCoordinates';
+import { assignCanvasMouseEvents } from '../tools/toolkit/buttonClickEvents/facade';
+import assignResetCanvasEventsFuncToMouseEvents from '../tools/toolkit/buttonClickEvents/facadeWorkersUtils/resetCanvasEvents';
 import { setBoundingBoxEditToolsToBeTransparent, createNewCanvas, reasignCanvas } from './utils/fabricUtils';
+import { assignCanvasForLabelManipulation } from './objects/label/label';
+import { assignCanvasForShapeFillManipulation } from './objects/allShapes/allShapes';
+import { assignCanvasForResettingToDefaultAfterAddPoints } from './mouseInteractions/mouseEvents/resetCanvasUtils/resetCanvasAfterAddPoints';
+import assignCanvasForResizeWhenWindowResize from '../tools/globalStyling/windowResize';
+import { assignCanvasForLabelAndShapeBuilder } from './objects/allShapes/labelAndShapeBuilder';
+import { assignCanvasToDimWindowService } from '../tools/dimWindow/dimWindowService';
+import { initialiseZoomVariables } from '../tools/toolkit/buttonClickEvents/facadeWorkers/zoomWorker';
 import { assignCanvasForUtils, assignNewCanvasForUtils } from './utils/canvasUtils';
+import { assignCanvasForSettingsPopup } from '../tools/settingsPopup/buttonClickEvents';
+import { assignCanvasForHotKeys } from '../keyEvents/keyboard/hotKeys';
+import assignCanvasForImageList from '../tools/imageList/canvasManagement';
 
 let currentCanvasInstance = null;
 
-function repopulateCanvasReference() {
+function repopulateCanvasReference(canvas) {
+  assignCanvasMouseEvents(canvas);
+  assignCanvasForHotKeys(canvas);
+  assignCanvasToDimWindowService(canvas);
+  assignCanvasForLabelManipulation(canvas);
+  assignCanvasForShapeFillManipulation(canvas);
+  assignResetCanvasEventsFuncToMouseEvents(canvas);
+  assignCanvasForResettingToDefaultAfterAddPoints(canvas);
+  assignCanvasForDrawingShapesViaCoordinates(canvas);
+  assignCanvasForResizeWhenWindowResize(canvas);
+  assignCanvasForLabelAndShapeBuilder(canvas);
+  assignCanvasForSettingsPopup(canvas);
+  assignCanvasForImageList(canvas);
+  initialiseZoomVariables(canvas);
 }
 
 function constructCanvas() {
