73111f99419767018c5a66c11d7e86f0eb4784b0
OvidijusParsiunas <oparsiunas@googlemail.com>
2020-08-07 22:09:21 +0000

    added an arbitrary deploy folder so that github would deploy all new content to the destination folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,64 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const FailOnErrorsPlugin = require('fail-on-errors-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CNAMEWebpackPlugin = require('cname-webpack-plugin');
 const path = require('path');
 
 const env = process.env.NODE_ENV || 'development';
 
 module.exports = () => {
-  const outputFileName = env === 'production' ? '[name].[contenthash].js' : '[name].js';
-  const externalsFileExtension = env === 'production' ? '.min.js' : '.js';
-  const plugins = [
+  let plugins = [];
+  let config = {};
+  if (env === 'production') {
+    config = {
+      loggingLevel: 'normal',
+      outputDirectory: 'tempDeployFolder',
+      outputFileName: '[name].[contenthash].js',
+      externalsFileExtension: '.min.js',
+      externalsDirectory: 'externalsProd',
+    };
+    plugins = plugins.concat([
+      new CleanWebpackPlugin(),
+      new CNAMEWebpackPlugin({
+        domain: 'myvision.ai',
+      })]);
+  } else {
+    config = {
+      loggingLevel: { assets: false, modules: false, children: false },
+      outputDirectory: 'publicDev',
+      outputFileName: '[name].js',
+      externalsFileExtension: '.js',
+      externalsDirectory: 'externalsDev',
+    };
+  }
+  plugins = plugins.concat([
     new FailOnErrorsPlugin({
       failOnErrors: true,
       failOnWarnings: true,
     }),
     new HtmlWebpackPlugin({
-      externalsFileExtension,
+      externalsFileExtension: config.externalsFileExtension,
       template: 'src/devIndexTemplate.html',
       minify: false,
-    })];
-  if (env === 'production') {
-    plugins.push(new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ['./public/*'],
-    }));
-  }
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/assets/css', to: 'assets/css' },
+        { from: './src/assets/images', to: 'assets/images' },
+        { from: './src/assets/svg', to: 'assets/svg' },
+        { from: `./src/assets/externals/${config.externalsDirectory}`, to: 'assets/externals' },
+      ],
+    }),
+  ]);
   return {
     entry: {
       browserSupportBundle: './src/browserSupport/index.js',
       appBundle: './src/app/index.js',
     },
     output: {
-      filename: outputFileName,
-      path: path.resolve(__dirname, './public'),
+      filename: config.outputFileName,
+      path: path.resolve(__dirname, `./${config.outputDirectory}`),
     },
     externals: {
       fabric: 'fabric',
@@ -55,6 +83,7 @@ module.exports = () => {
       ],
     },
     mode: env,
+    stats: config.loggingLevel,
     plugins,
     performance: {
       maxEntrypointSize: 340000,
