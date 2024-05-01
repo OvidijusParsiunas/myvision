56c818f4c07a14340b1153477b86f3ab8302fa42
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 00:28:36 +0600

    comment

--- src/app/tools/uploadDatasetsModal/views/uploadDatasets/datasetObjectManagers/CSVDatasetObjectManager.js
+++ src/app/tools/uploadDatasetsModal/views/uploadDatasets/datasetObjectManagers/CSVDatasetObjectManager.js
@@ -1,6 +1,10 @@
+// Import the DatasetObjectManagerBuilder module from './builders/datasetObjectManagerBuilder'
 import DatasetObjectManagerBuilder from './builders/datasetObjectManagerBuilder';
 
+// Create an instance of the object manager using the buildObjectManagerForOneAnnotationFileStrategy method
 const datasetObjectManager = DatasetObjectManagerBuilder
   .buildObjectManagerForOneAnnotationFileStrategy();
 
+// Export the datasetObjectManager instance as the default export
 export default datasetObjectManager;
+
