bd228d7a74cd60d1c798e9607985c9d0d44ff8de
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 00:28:36 +0600

    comment

--- src/app/tools/toolkit/buttonClickEvents/facadeWorkers/toggleExportDatasetsPopUpWorker.js
+++ src/app/tools/toolkit/buttonClickEvents/facadeWorkers/toggleExportDatasetsPopUpWorker.js
@@ -1,12 +1,21 @@
+// Importing the displayExportDatasetsPopup and hideExportDatasetsPopup functions from the style module of exportDatasetsPopup.
 import { displayExportDatasetsPopup, hideExportDatasetsPopup } from '../../../exportDatasetsPopup/style';
+
+// Importing the getExportDatasetsPopupOpenState function from the state module of the current directory.
 import { getExportDatasetsPopupOpenState } from '../../../state';
 
+// The toggleExportDatasetsPopup function is used to toggle the visibility of the export datasets popup.
 function toggleExportDatasetsPopup() {
+  // Checking if the export datasets popup is currently closed.
   if (!getExportDatasetsPopupOpenState()) {
+    // If the popup is closed, display it using the displayExportDatasetsPopup function.
     displayExportDatasetsPopup();
   } else {
+    // If the popup is already open, hide it using the hideExportDatasetsPopup function.
     hideExportDatasetsPopup();
   }
 }
 
+// Exporting the toggleExportDatasetsPopup function as the default export of this module.
 export { toggleExportDatasetsPopup as default };
+
