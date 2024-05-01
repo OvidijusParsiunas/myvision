5e8e18528a5ad63d29e114e7f1e221a0d0c9eb7c
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 00:28:56 +0600

    comment

--- src/app/canvas/mouseInteractions/mouseEvents/eventWorkers/removePointsOnNewPolygonEventsWorker.js
+++ src/app/canvas/mouseInteractions/mouseEvents/eventWorkers/removePointsOnNewPolygonEventsWorker.js
@@ -3,25 +3,40 @@ import {
 } from '../../../objects/polygon/alterPolygon/alterPolygon';
 import { getCurrentlyHoveredDrawPoint } from '../../../objects/polygon/polygon';
 
+// Flag to keep track of whether points are being removed or not
 let removingPoints = false;
+// The canvas object that the points belong to
 let canvas = null;
+// The point that is currently being hovered over
 let currentlyHoveredPoint = null;
+// A flag to ignore the first mouse movement event
 let ignoredFirstMouseMovement = false;
+// The point that was last hovered over
 let lastHoveredPoint = null;
+// A flag to keep track of whether the mouse has moved since the last point hover
 let mouseMoved = false;
 
+/**
+ * Sets up the event listeners and initializes the state for the canvas and points
+ * @param {Object} canvasObj - The canvas object that the points belong to
+ * @param {Object} polygonObj - The polygon object to make editable
+ */
 function setRemovablePointsEventsCanvas(canvasObj, polygonObj) {
   canvas = canvasObj;
   ignoredFirstMouseMovement = false;
   currentlyHoveredPoint = null;
   lastHoveredPoint = null;
   mouseMoved = false;
-  // edit this
+  // If a polygon object is provided, make it editable
   if (polygonObj) {
     setEditablePolygon(canvas, polygonObj, true, true);
   }
 }
 
+/**
+ * Removes the point that was clicked on
+ * @param {Object} event - The mouse down event object
+ */
 function pointMouseDownEvents(event) {
   if (event.target && event.target.shapeName === 'point') {
     removePolygonPoint(event.target.pointId);
@@ -29,6 +44,9 @@ function pointMouseDownEvents(event) {
   }
 }
 
+/**
+ * Removes the last hovered point via the keyboard
+ */
 function removeTempPointViaKeyboard() {
   if (!mouseMoved) {
     if (lastHoveredPoint) {
@@ -47,6 +65,10 @@ function removeTempPointViaKeyboard() {
   currentlyHoveredPoint = null;
 }
 
+/**
+ * Highlights the point that is being hovered over
+ * @param {Object} event - The mouse over event object
+ */
 function pointMouseOverEvents(event) {
   if (event.target && event.target.shapeName === 'point' && event.target.fill === 'red') {
     event.target.stroke = 'red';
@@ -55,21 +77,31 @@ function pointMouseOverEvents(event) {
   }
 }
 
+/**
+ * Filler function for the default parent call
+ */
 function pointMouseUpEvents() {
-  // filler function for the default parent call
+  // Filler function for the default parent call
 }
 
+/**
+ * Un-highlights the point that was previously hovered over
+ * @param {Object} event - The mouse out event object
+ */
 function pointMouseOutEvents(event) {
   if (event.target && event.target.shapeName === 'point') {
     event.target.stroke = 'black';
     canvas.renderAll();
     currentlyHoveredPoint = null;
-    // fix for the bug where upon hovering over a point in another mode and switching it to this
+    // Fix for the bug where upon hovering over a point in another mode and switching it to this
     // mode - the mouse out event is triggered, highlighting the last hovered shape
     if (!mouseMoved) lastHoveredPoint = event.target;
   }
 }
 
+/**
+ * Ignores the first mouse movement event and sets the mouseMoved flag to true
+ */
 function pointMouseMoveEvents() {
   if (ignoredFirstMouseMovement) {
     mouseMoved = true;
@@ -78,10 +110,17 @@ function pointMouseMoveEvents() {
   }
 }
 
+/**
+ * Returns the removingPoints flag
+ * @returns {boolean} - The removingPoints flag
+ */
 function getRemovingPointsState() {
   return removingPoints;
 }
 
+/**
+ * Sets the removingPoints flag to false
+ */
 function setRemovingPointsStateToFalse() {
   removingPoints = false;
 }
